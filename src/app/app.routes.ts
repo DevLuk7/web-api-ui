32e664dc0a9dccdd8a8e878b7561829f9f4a100f
DevLuk7 <lukass100@gmail.com>
2024-03-28 22:27:06 +0100

    remove login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,12 @@
 import { Route } from '@angular/router';
 import { authGuardFn } from '@auth0/auth0-angular';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { LoginPageComponent } from './login-page/login-page.component';
 
 export const appRoutes: Route[] = [
   {
     path: '',
     redirectTo: 'dashboard',
     pathMatch: 'full',
-  },
-  {
-    path: 'login',
-    component: LoginPageComponent,
   },
   {
     path: 'dashboard',
