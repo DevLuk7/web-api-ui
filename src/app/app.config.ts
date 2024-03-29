bc44a45d312d4ca9de14f3e31ec091843db27abf
DevLuk7 <lukass100@gmail.com>
2024-03-29 21:30:27 +0100

    update config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,10 @@
-import { DOCUMENT } from '@angular/common';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { ApplicationConfig, importProvidersFrom, inject } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
 import { authHttpInterceptorFn, provideAuth0 } from '@auth0/auth0-angular';
 import { ApiModule, Configuration, ConfigurationParameters } from './api';
 import { appRoutes } from './app.routes';
-
-const document = inject(DOCUMENT);
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -19,7 +16,7 @@ export const appConfig: ApplicationConfig = {
       domain: 'dev-q70qohzvtsuc0rmo.us.auth0.com',
       clientId: 'K3NxUUmC9ddETdtCXgcwmf30KffvBC1d',
       authorizationParams: {
-        redirect_uri: document.location.origin,
+        redirect_uri: location.origin,
         audience: 'http://localhost:5145',
       },
       httpInterceptor: {
