7f93ba776cb90b07933d52b7beee56db99f0cfae
DevLuk7 <lukass100@gmail.com>
2024-03-29 15:12:18 +0100

    remove SSR

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, MatToolbarModule, MatButtonModule],
+  imports: [RouterModule, CommonModule, MatToolbarModule, MatButtonModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  private readonly authService = inject(AuthService);
+  readonly user$ = this.authService.user$;
+
+  logout() {
+    this.authService.logout();
+  }
+}
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,5 @@
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
 import { authHttpInterceptorFn, provideAuth0 } from '@auth0/auth0-angular';
@@ -9,7 +8,7 @@ import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideClientHydration(),
+    // provideClientHydration(),
     provideRouter(appRoutes),
     provideHttpClient(withInterceptors([authHttpInterceptorFn])),
     provideAnimationsAsync(),
