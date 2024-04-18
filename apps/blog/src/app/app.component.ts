8eda9cb05beea1c11b1b039ad4511b83b8a373e2
DevLuk7 <lukass100@gmail.com>
2024-04-18 18:55:51 +0200

    create footer

--- apps/blog/src/app/app.component.ts
+++ apps/blog/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from './ui/footer/footer.component';
 import { NavigationComponent } from './ui/navigation/navigation.component';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, NavigationComponent],
+  imports: [RouterModule, NavigationComponent, FooterComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
