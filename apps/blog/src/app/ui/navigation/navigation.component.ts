4473c3a1c317e691dea2ea831d61aa0944406460
DevLuk7 <lukass100@gmail.com>
2024-04-19 09:04:08 +0200

    change styles add more content

--- apps/blog/src/app/ui/navigation/navigation.component.ts
+++ apps/blog/src/app/ui/navigation/navigation.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
