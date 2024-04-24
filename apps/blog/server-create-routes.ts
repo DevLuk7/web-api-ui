78a6297765435fbe8cb0ede0106ee2689bdbf891
DevLuk7 <lukass100@gmail.com>
2024-04-24 21:23:49 +0200

    add SSG to posts

---
+++ apps/blog/server-create-routes.ts
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+async function createFile() {
+  const response = await fetch('https://webapi-api-app.azurewebsites.net/api/Post');
+  const posts = await response.json();
+  fs.writeFileSync('apps/blog/routes.txt', posts.map((x: any) => `/post/${x.id}`).join('\n'));
+}
+
+createFile();
