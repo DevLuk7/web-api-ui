dfce1fa5e5ba9779de4c2de9a5b6931481a75b2c
DevLuk7 <lukass100@gmail.com>
2024-06-11 17:14:56 +0200

    remove old code, coment conde

--- apps/blog/src/app/app.config.ts
+++ apps/blog/src/app/app.config.ts
@@ -1,20 +1,17 @@
 import { DATE_PIPE_DEFAULT_OPTIONS } from '@angular/common';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { ApplicationConfig, importProvidersFrom, isDevMode } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
-import { ApiModule, Configuration, ConfigurationParameters } from '@web-api-ui/web-api';
 import { appRoutes } from './app.routes';
 
 export const config = () => {
   if (isDevMode()) {
     return {
-      apiUrl: 'http://localhost:3333',
       cmsUrl: 'http://localhost:1337',
     };
   }
   return {
-    apiUrl: 'https://api.devluk.io',
     cmsUrl: 'https://cms.devluk.io',
   };
 };
@@ -24,14 +21,6 @@ export const appConfig: ApplicationConfig = {
     provideClientHydration(),
     provideHttpClient(withFetch()),
     provideRouter(appRoutes),
-    importProvidersFrom(
-      ApiModule.forRoot(() => {
-        const params: ConfigurationParameters = {
-          basePath: config().apiUrl,
-        };
-        return new Configuration(params);
-      })
-    ),
     {
       provide: DATE_PIPE_DEFAULT_OPTIONS,
       useValue: {
--- apps/blog/src/app/data-access/cms.service.ts
+++ apps/blog/src/app/data-access/cms.service.ts
@@ -4,10 +4,6 @@ import { Observable, map } from 'rxjs';
 import { config } from '../app.config';
 import { Post } from './post.model';
 
-const headers = {
-  Authorization:
-    'Bearer 15d40688cc9ece96ab1cc59a26ea411de6069bc3927533b7fd3e859931d4f1b61a67ee5c241ed60c03a748f47ace0fe6a659fde04482923b8355b112b25fab3b83938f381f85182bbbaae0fe7e20aabad9df986b9789a084a2953b46c6b5541f9a8f43dfc28efd18a6dece24af339cfc93d9a7d4f6a81d7fd9659575074cdbde',
-}
 
 @Injectable({ providedIn: 'root' })
 export class CMSService {
@@ -16,17 +12,13 @@ export class CMSService {
 
   getPosts(): Observable<Post[]> {
     return this.http
-      .get(`${this.cmsUrl}/api/posts`, {
-        headers
-      })
+      .get(`${this.cmsUrl}/api/posts`)
       .pipe(map((res: any) => res.data));
   }
 
   getPost(slug: string): Observable<Post> {
     return this.http
-      .get(`${this.cmsUrl}/api/posts?filters[slug][$eq]=${slug}`, {
-        headers
-      })
+      .get(`${this.cmsUrl}/api/posts?filters[slug][$eq]=${slug}`)
       .pipe(map((res: any) => res.data[0]));
   }
 }
